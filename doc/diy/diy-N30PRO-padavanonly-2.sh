e3cf7ed916d46ea2b8e04a54f7efd28789ae7003
HuiDong Netwrok <muxuejin@outlook.com>
2023-06-30 23:26:07 +0800

    Update diy-N30PRO-padavanonly-2.sh

--- doc/diy/diy-N30PRO-padavanonly-2.sh
+++ doc/diy/diy-N30PRO-padavanonly-2.sh
@@ -9,6 +9,8 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# 修改默认IP
+sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
 sed -i "s/DISTRIB_DESCRIPTION=.*/DISTRIB_DESCRIPTION='OpenWrt By HuiDong '/g" package/base-files/files/etc/openwrt_release
 # 移除重复软件包
 #rm -rf feeds/luci/themes/luci-theme-argon
